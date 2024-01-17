aeb92ff98cc8487e22ede91bb3b7c38517d07f32
Your Name <you@example.com>
2024-01-17 16:16:22 +0530

    string function

---
+++ prectice.php
@@ -0,0 +1,39 @@
+<?php
+//strlen() function prectice
+
+//  $str = "welcome to the world";
+//     echo  strlen("");
+//     echo strlen($str);
+// this function return the lengh of a given string. it takes a string as a parameter and return its length.
+
+
+
+// str_replace prectice
+
+//echo str_replace("world","tirtha","hello world");
+// echo str_replace("happy","about","together");
+// function replace characters with some other caracters in string.
+
+//strpos function
+
+// echo strpos("hello my world, i love php","php");
+// echo stripos("hello my world, i love php","php");
+// echo strripos("exploring is best,my life is best","best");
+
+// find the position of the first occurrence of "php" inside the string and strripos find the position of last occurrence of a string inside the another string.
+
+
+// substr() function
+
+
+// echo substr("hello world",6);
+// echo substr("hello world",3);
+// echo substr("hello world", 10);
+// echo substr("hello tirtha", 5);
+//echo substr("hello world",-4);
+//echo substr("hello world",-2);
+//echo substr ("hello world",0);
+
+// this function returns a part of a string. start is re
+
+?>
