9079902ad852af16aacd7493fb9cfad88264d2ba
Your Name <you@example.com>
2024-01-17 18:25:19 +0530

    update

--- prectice.php
+++ prectice.php
@@ -33,7 +33,51 @@
 //echo substr("hello world",-4);
 //echo substr("hello world",-2);
 //echo substr ("hello world",0);
+//echo substr ("hello world",-4);
+//echo substr("hello world",-6);
 
-// this function returns a part of a string. start is re
+// this function returns a part of a string. start is return a parameter.
 
+
+// echo strtoupper("tirtha");
+
+// echo strtolower("TIRTHA");
+
+// $str = "hello world";
+// //echo trim($str,"hed");
+// echo trim($str,"hello" );
+
+// echo "without trim" .$str;
+// echo "with trim" .trim($str);
+
+//  $arr = array('hello','world','beautiful','day');
+//  echo implode(" ",$arr);
+//   echo implode("+",$arr);
+//   echo implode("-",$arr);
+ // return a string from the elements of an array
+
+
+ //explode() funtion
+
+//  $str ="hello ,world, its,beautiful,day,22";
+//  //print_r (explode(" ",$str));
+//  print_r(explode(",",$str,2));
+// print_r(explode(",",$str,4))
+
+//explode function break  string into array 
+
+  
+
+//
+
+//echo str_repeat("tirtha",20);
+
+//string repted 
+
+//echo strrev("tirtha shah");
+
+//echo str_shuffle("hello this is")
 ?>
+
+
+
