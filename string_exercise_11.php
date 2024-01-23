d09907e5dcc309402027b0c37366566226f9067f
Your Name <you@example.com>
2024-01-23 12:03:02 +0530

    bubblesort

--- string_exercise_11.php
+++ string_exercise_11.php
@@ -1,24 +1,25 @@
-<!-- <?php
-$a=10;
-for($i=1; $i<= $a; $i++){
-    // for($j=1; $j<=$a; $j++){
-    //     if($i<=$j){
-    //         echo("$j");
-      }
-    }
-    echo "<br>";
-} ?> -->
-
 <?php
-$n=10;
-for($i=1;$i<=$n;$i++){
+function bubbleSort($array) {
+    $n = count($array);
 
-    for($j=1;$j<=$n;$j++){
-        if($j<=($n+1)-$i){
-        echo $j." ";
+    for ($i = 0; $i < $n - 1; $i++) {
+        //$n-$i-$1 // Avoid comparing the last element in each pass, as it's already sorted as per bubble sort working.
+        for ($j = 0; $j < $n - $i - 1; $j++) {
+            // Swap if the element found is greater than the next element
+            if ($array[$j] > $array[$j + 1]) {
+                $temp = $array[$j];
+                $array[$j] = $array[$j + 1];
+                $array[$j + 1] = $temp;
+            }
         }
     }
-    echo "<br>";
+    return $array;
 }
 
+
+$arrayToSort = [64, 34, 25, 12, 22, 11, 90];
+$sortedArray = bubbleSort($arrayToSort);
+
+echo "Original array: " . implode(", ", $arrayToSort) . "<br>";
+echo "Sorted array: " . implode(", ", $sortedArray) . "<br>";
 ?>
