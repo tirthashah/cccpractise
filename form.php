437546df7f747c1034a63cb5543eb973e7df687c
Your Name <you@example.com>
2024-01-23 14:50:12 +0530

    pattern

--- form.php
+++ form.php
@@ -4,40 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Product Information Form</title>
-    <style>
-        body {
-            font-family: Arial, sans-serif;
-            padding: 20px;
-        }
-        form {
-            max-width: 600px;
-            margin: auto;
-        }
-        label {
-            display: block;
-            margin-bottom: 5px;
-        }
-        input, select {
-            width: 100%;
-            padding: 8px;
-            margin-bottom: 10px;
-            box-sizing: border-box;
-        }
-        input[type="date"] {
-            width: calc(100% - 18px); / Adjust for date input width /
-        }
-        input[type="submit"] {
-            background-color: #4CAF50;
-            color: white;
-            padding: 10px 15px;
-            border: none;
-            border-radius: 4px;
-            cursor: pointer;
-        }
-        input[type="submit"]:hover {
-            background-color: #45a049;
-        }
-    </style>
+   
 </head>
 <body>
     <form action="insert.php" method="post">
--- string_exercise_7.php
+++ string_exercise_7.php
@@ -1,4 +1,4 @@
-<?php
+<!-- <?php
 
 
 // for ($i = 1; $i <= 100; $i++) {
@@ -42,4 +42,23 @@ for($a = 1; $a <= 50 ; $a++)
 
 
 
-?>
+// ?>
+
+
+
+
+
+<!-- <//?php
+
+// Nested for loops to print numbers from 1 to 10
+// for ($i = 1; $i <= 10; $i++) {
+//     echo "outer loop : $i ";
+//     echo "<br>";
+//     for ($j = 10; $j >=1; $j--) {
+//         echo "inner loop : $j ";
+//         echo "<br>";
+//     }
+//     echo "<br>";
+//     echo "<br>";
+//} -->
+
